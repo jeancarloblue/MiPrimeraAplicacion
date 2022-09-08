ca88716fd4eeaaf564bd97cddec377a64723d4da
Antonio Fdez <antonifernandez7@hotmail.com>
2022-09-08 21:18:41 +0200

    Mi Primera Aplicacion

---
+++ Data/MiPrimeraAplicacionContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MiPrimeraAplicacion.Models;
+
+namespace MiPrimeraAplicacion.Data
+{
+    public class MiPrimeraAplicacionContext : DbContext
+    {
+        public MiPrimeraAplicacionContext (DbContextOptions<MiPrimeraAplicacionContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<MiPrimeraAplicacion.Models.Cliente> Cliente { get; set; }
+    }
+}
---
+++ Models/Cliente.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MiPrimeraAplicacion.Models
+{
+    public class Cliente
+    {
+        public int ClienteId { get; set; }
+        public string Nombre { get; set; }
+        public string Email { get; set; }
+
+    }
+}
